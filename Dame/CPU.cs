f9f7d06bb9029112512a04f74578330e60b79e4e
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-01 21:18:30 +0200

    CPU.cs begonnen

---
+++ Dame/CPU.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dame
+{
+    public class CPU
+    {
+        public string get_move(Dictionary<Tuple<int, int>, char> Boardstate)
+        {
+            string rr = "";
+
+            return rr;
+        }
+
+    }
+}
