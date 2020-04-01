25abc41e2450b8040e73edee72f8cb7f03c43d82
Errorihatewindows <Daily-craft@hotmail.de>
2020-04-01 04:40:30 +0200

    asdf

--- Dame/MCP.cs
+++ Dame/MCP.cs
@@ -21,10 +21,10 @@ namespace Dame
             else { top = 'b'; }
 
             //nur reihe 0-3 besetzt (und jeweils gespiegelt)
-            for (int y = 0; i < 3; i++)
+            for (int y = 0; y < 3; y++)
                 for (int x = 0; x < 8; x += 2)
                 {
-                    if (i % 2 == 1)
+                    if (y % 2 == 1)
                     {
                         Board.Add(Tuple.Create((x + 1), y), bot);
                         //gegnerische Steine
