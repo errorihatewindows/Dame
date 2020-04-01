3f6029e227e8966047f3321d00b1aac5c8bb0734
Errorihatewindows <Daily-craft@hotmail.de>
2020-04-01 18:04:48 +0200

    Move done

--- Dame/Form1.Designer.cs
+++ Dame/Form1.Designer.cs
@@ -239,7 +239,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(973, 40);
+            this.button1.Location = new System.Drawing.Point(590, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 83;
