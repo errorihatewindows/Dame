4c32bd0677efb4cd1c625707db9972ef4dec2917
Errorihatewindows <Daily-craft@hotmail.de>
2020-04-01 02:54:48 +0200

    Added Generate_board

--- Dame/MCP.cs
+++ Dame/MCP.cs
@@ -5,8 +5,34 @@ namespace MCP
 {
 	public class MCP
 	{
+		//Constructor
 		public MCP()
 		{
 		}
-	}
+
+        private void Generate_Board(char bot) //erstellt Startposition für weiß und schwarz + zeichnen
+        {
+            char top;
+            if (bot == 'b') {  top = 'w';  }
+            else            {  top = 'b';  }
+
+            //nur reihe 0-3 besetzt (und jeweils gespiegelt)
+            for (int y = 0; i < 3; i++)
+                for (int x = 0; x < 8; x += 2)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Board.Add(Tuple.Create((x + 1), y), bot);
+                        //gegnerische Steine
+                        Board.Add(Tuple.Create(x, (7 - y)), top);
+                    }
+                    else
+                    {
+                        Board.Add(Tuple.Create(x, y), bot);
+                        Board.Add(Tuple.Create((x + 1), (7 - y)), top);
+                    }
+                }
+
+        }
+    }
 }
