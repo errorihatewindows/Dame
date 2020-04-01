e7bc4d67297fee612a61d651500d79119b7c873f
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-01 22:17:40 +0200

    cpu.cs

--- Dame/CPU.cs
+++ Dame/CPU.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Piece = System.Tuple<int, int>;
+using Board = System.Collections.Generic.Dictionary<System.Tuple<int, int>, char>;
 
 namespace Dame
 {
     public class CPU
     {
-        public string get_move(Dictionary<Tuple<int, int>, char> Board)
+        public string get_move(Board Board, int player)               // int = 0 => Schwarz (unten), fängt an
         {
-            string rr = "";
+            foreach (KeyValuePair<Piece, char> kvp in Board)
+            {
+                
+            }
+                
 
-            return rr;
+
+
+                return;
         }
 
     }
