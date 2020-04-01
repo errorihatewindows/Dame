4c800245da1f33364b9fed4b04b0ca784caf32dc
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-01 23:13:33 +0200

    Anfang CPU.cs

--- Dame/CPU.cs
+++ Dame/CPU.cs
@@ -15,19 +15,24 @@ namespace Dame
         List<Piece> possible = new List<Piece>();
         string final_move;
 
-        
+        //Konstruktor
+        public CPU(Form1 form)
+        {
+
+        }
 
         public string get_move(Board Board, int player)               // int = 0 => Schwarz (unten), fängt an
         {
             output.Clear();
             possible.Clear();
 
-
+            
             foreach (KeyValuePair <Piece,char> position in Board)
             {
                 //Computer hat Schwarz + aktueller Stein ist Schwarz
-                if (player == 0 && (position.Value == 'b' || position.Value == 'B'))
+                if ((player == 0) && ((position.Value == 'b') || (position.Value == 'B')))                  
                     checkAll_Black(position.Key);
+               
 
                 //Computer hat Weiß + aktueller Stein ist Weiß
                 if (player == 1 && (position.Value == 'w' || position.Value == 'W'))
@@ -45,6 +50,8 @@ namespace Dame
         private List<Piece> checkAll_Black(Piece piece)
         {
             //mögliche diagonalen nach oben
+
+            Console.WriteLine("Black");
             possible.Add(Tuple.Create(piece.Item1 + 1, piece.Item2 + 1));
             possible.Add(Tuple.Create(piece.Item1 - 1, piece.Item2 + 1));
 
@@ -54,6 +61,8 @@ namespace Dame
         private List<Piece> checkAll_White(Piece piece)
         {
             //mögliche Diagonalen nach unten
+
+            Console.WriteLine("White");
             possible.Add(Tuple.Create(piece.Item1 + 1, piece.Item2 - 1));
             possible.Add(Tuple.Create(piece.Item1 - 1, piece.Item2 - 1));
 
