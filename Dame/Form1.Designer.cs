4a060135b9c52cb9c563057ace4fad0b753d8e57
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-10 21:50:00 +0200

    d

--- Dame/Form1.Designer.cs
+++ Dame/Form1.Designer.cs
@@ -619,7 +619,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(247, 23);
             this.button2.TabIndex = 107;
-            this.button2.Text = "Let´s make Love";
+            this.button2.Text = "learn";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
